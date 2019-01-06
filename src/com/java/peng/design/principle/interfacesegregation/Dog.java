055db6cb4b3e368a84c7791c997e6bcf238b705c
WhyMEliu <527453497@qq.com>
2019-01-06 17:48:46 +0800

    Interface segregation

---
+++ src/com/java/peng/design/principle/interfacesegregation/Dog.java
@@ -0,0 +1,17 @@
+package com.java.peng.design.principle.interfacesegregation;
+
+public class Dog implements IEatAnimalAction,ISwimAnimalAction {
+
+	@Override
+	public void swim() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void eat() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
---
+++ src/com/java/peng/design/principle/interfacesegregation/IEatAnimalAction.java
@@ -0,0 +1,5 @@
+package com.java.peng.design.principle.interfacesegregation;
+
+public interface IEatAnimalAction {
+	void eat();
+}
