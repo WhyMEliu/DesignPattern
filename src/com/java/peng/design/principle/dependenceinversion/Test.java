ffe0d3cc802c27680f802594c60b91f9b02e0247
WhyMEliu <527453497@qq.com>
2019-01-30 17:36:38 +0800

    Update

--- src/com/java/peng/design/principle/dependenceinversion/Test.java
+++ src/com/java/peng/design/principle/dependenceinversion/Test.java
@@ -1,9 +1,11 @@
 package com.java.peng.design.principle.dependenceinversion;
 
 //依赖倒置原则
-//高层不依赖底层实现
-//面向接口编程
-//同时符合开闭原则
+//定义：高层模块不应该依赖底层模块，二者都应该依赖其抽象
+//	抽象不应该依赖细节；细节应该依赖抽象
+//	针对接口编程，不要针对实现编程
+
+//优点：可以减少类间的耦合性，提高稳定性，提高代码的可读性和可维护性，可降低修改程序所造成的风险
 public class Test {
 	public static void main(String[] args) {
 		Peng peng = new Peng();
--- src/com/java/peng/design/principle/openclose/Test.java
+++ src/com/java/peng/design/principle/openclose/Test.java
@@ -1,8 +1,10 @@
 package com.java.peng.design.principle.openclose;
 
 //开闭原则
-//开：对扩展开放
-//闭：对原来的代码修改关闭
+//定义：一个软件实体如类，模块和函数应该对扩展开放，对修改关闭
+//	用抽象构建框架，用实现扩展细节
+
+//优点：提高软件系统的可复用性及可维护性
 public class Test {
 	public static void main(String[] args) {
 		ICourse iCourse = new JavaDiscountCourse(001, "Hello World~", 300d);
